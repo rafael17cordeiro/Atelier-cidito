5a2de5e11dbbc0aa6222195214c6db0ebfdb720c
rafael17cordeiro <59150464+rafael17cordeiro@users.noreply.github.com>
2025-09-22 22:56:19 +0100

    Base site structure

---
+++ Js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-name", {
+        strings: ["Estofagem & reparação", "Conforto e proteção para as suas malas", "Estofos que protegem e impressionam"
+            , "Manutenção e restauro de qualidade", "Damos nova vida à sua mala"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 3000,
+        loop: true,
+        showCursor: true,
+        cursorChar: "|",
+    });
+});
+
+
+
+

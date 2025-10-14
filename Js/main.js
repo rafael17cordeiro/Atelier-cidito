6ab3388812efbd12b9cf6adb4dda18c27fe3785c
Rafael Cordeiro <59150464+rafael17cordeiro@users.noreply.github.com>
2025-10-14 16:16:42 +0100

    Minor changes

--- Js/main.js
+++ Js/main.js
@@ -1,16 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    new Typed("#typed-name", {
-        strings: ["Estofagem & reparação", "Conforto e proteção para as suas malas", "Estofos que protegem e impressionam"
-            , "Manutenção e restauro de qualidade", "Damos nova vida à sua mala"],
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 3000,
-        loop: true,
-        showCursor: true,
-        cursorChar: "|",
-    });
+AOS.init();
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Thumbs: false,
+  transitionEffect: "fade", // fade, zoom-in-out, slide, circular
+  transitionDuration: 500, // em milissegundos
+  Toolbar: {
+    display: [{ id: "counter", position: "center" }, "zoom", "close"],
+  },
 });
-
-
-
-
